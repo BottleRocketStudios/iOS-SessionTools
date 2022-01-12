7bade4e77e3fca416df1dd1ffeaea0e42ec4f3ff
Will McGinty <will.mcginty@bottlerocketstudios.com>
2022-01-12 17:20:54 -0600

    Progress with project restructure

--- SessionTools/README.md
+++
@@ -1,3 +0,0 @@
-# SessionTools
-
-A description of this package.
---
+++ Sources/SessionTools.h
@@ -0,0 +1,18 @@
+//
+//  SessionTools.h
+//  SessionTools
+//
+//  Created by Will McGinty on 1/12/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SessionTools.
+FOUNDATION_EXPORT double SessionToolsVersionNumber;
+
+//! Project version string for SessionTools.
+FOUNDATION_EXPORT const unsigned char SessionToolsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SessionTools/PublicHeader.h>
+
+
