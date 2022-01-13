72af65c6ff3425c57df634c91124fa29944d50bf
Will McGinty <will.mcginty@bottlerocketstudios.com>
2022-01-13 11:28:41 -0600

    Clean up Package.swift

--- Package.swift
+++ Package.swift
@@ -4,27 +4,19 @@ import PackageDescription
 let package = Package(
     name: "SessionTools",
     platforms: [
-        .macOS("10.12"),
-        .iOS("10.0"),
-        .tvOS("10.0"),
-        .watchOS("4.2")
+        .macOS(.v10_12),
+        .iOS(.v10),
+        .tvOS(.v10),
+        .watchOS("4.2"),
     ],
     products: [
-        .library(
-            name: "SessionTools",
-            targets: ["SessionTools"])
+        .library(name: "SessionTools", targets: ["SessionTools"])
     ],
     dependencies: [
         .package(url: "https://github.com/kishikawakatsumi/KeychainAccess.git", from: "4.2.0"),
     ],
     targets: [
-        .target(
-            name: "SessionTools",
-            dependencies: ["KeychainAccess"],
-            path: "Sources"),
-        .testTarget(
-            name: "SessionTools-iOSTests",
-            dependencies: ["SessionTools"],
-            path: "Tests")
+        .target(name: "SessionTools", dependencies: ["KeychainAccess"], path: "Sources"),
+        .testTarget(name: "SessionToolsTests", dependencies: ["SessionTools"], path: "Tests")
     ]
 )
